3335beaf8bc7045216f819555bc0a64cfd9c360e
GrantBerland <grant.berland.gb@gmail.com>
2021-06-04 15:16:57 -0600

    fixed messenger command

--- src/SteppingActionMessenger.cc
+++ src/SteppingActionMessenger.cc
@@ -20,12 +20,12 @@ SteppingActionMessenger::SteppingActionMessenger(SteppingAction* step)
   fcmd->SetDefaultValue(0);
   fcmd->AvailableForStates(G4State_PreInit, G4State_Idle);
 
-  fcmd2 = new G4UIcmdWithADouble("/dataCollection/setMinimumPhotonWindow",this);
+  fcmd2 = new G4UIcmdWithADouble("/dataCollection/SetMinPhotonWindow",this);
   fcmd2->SetParameterName("Minimum photon tracking altitude [km]", true);
   fcmd2->SetDefaultValue(250.);
   fcmd2->AvailableForStates(G4State_PreInit, G4State_Idle);
   
-  fcmd3 = new G4UIcmdWithADouble("/dataCollection/setMinimumPhotonWindow",this);
+  fcmd3 = new G4UIcmdWithADouble("/dataCollection/SetMaxPhotonWindow",this);
   fcmd3->SetParameterName("Maximum photon tracking altitude [km]", true);
   fcmd3->SetDefaultValue(275.);
   fcmd3->AvailableForStates(G4State_PreInit, G4State_Idle);
